2de4af84ab39fc591ac9dff09fa0efd995fc0d79
Secumfex <Arend.Buchacher@gmx.de>
2014-06-13 11:18:42 +0200

    #26 Manipulating Values :  added Utility Listeners
    - added some generic Listeners which might come in handy later
    - UNTESTED, though

--- src/libraries/Utility/UtilityListeners.h
+++ src/libraries/Utility/UtilityListeners.h
@@ -2,6 +2,7 @@
 #define UTILITYLISTENERS_H
 
 #include <string>
+#include <vector>
 #include "SubjectListenerPattern.h"
 
 class InvertBooleanListener : public Listener
@@ -22,6 +23,43 @@ private:
 public:
 	DebugPrintDoubleListener( double* doublePtr , std::string message);
 	virtual ~DebugPrintDoubleListener();
+	void call();
+};
+
+template < class T >
+class IncrementValueListener : public Listener
+{
+private:
+	T* p_value;
+	T m_increment;
+public:
+	IncrementValueListener(T* valuePtr, T increment);
+	virtual ~IncrementValueListener();
+	void call();
+};
+
+template < class T >
+class DecrementValueListener : public Listener
+{
+private:
+	T* p_value;
+	T m_decrement;
+public:
+	DecrementValueListener(T* valuePtr, T decrement);
+	virtual ~DecrementValueListener();
+	void call();
+};
+
+template < class T >
+class SwitchThroughValuesListener : public Listener
+{
+private:
+	T* p_value;
+	std::vector < T& > m_candidates;
+	unsigned int m_activeCandidate;
+public:
+	SwitchThroughValuesListener( T* valuePtr, std::vector< T& > candidates);
+	virtual ~SwitchThroughValuesListener();
 	void call();
 };
 
